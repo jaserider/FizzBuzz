f4a0c124215e9ce10963fc389ada0e25b25eadc9
jaserider <jaserider@hotmail.com>
2024-10-07 11:11:32 +0100

    Version 3 - Tests passed

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,6 +6,23 @@
         {
             string result = string.Empty;
 
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                result = "Fizzbuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                result = "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                result = "Buzz";
+            }
+            else
+            {
+                result = number.ToString();
+            }
+
             return result;
         }
     }
