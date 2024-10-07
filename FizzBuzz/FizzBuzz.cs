1d91de45d8cdb7e164fbb58e5c1219761463bfa6
jaserider <jaserider@hotmail.com>
2024-10-07 11:43:06 +0100

    Version 5 - Separate rules classes into separate cs file

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -1,50 +1,5 @@
 ﻿namespace FizzBuzz
 {
-    public interface IRuleInterface
-    {
-        bool MatchesRule(int number);
-        string GetFizzBuzzString();
-    }
-
-    public class FizzRule : IRuleInterface
-    {
-        public bool MatchesRule(int number)
-        {
-            return number % 3 == 0;
-        }
-
-        public string GetFizzBuzzString()
-        {
-            return "Fizz";
-        }
-    }
-
-    public class BuzzRule : IRuleInterface
-    {
-        public bool MatchesRule(int number)
-        {
-            return number % 5 == 0;
-        }
-
-        public string GetFizzBuzzString()
-        {
-            return "Buzz";
-        }
-    }
-
-    public class FizzBuzzRule : IRuleInterface
-    {
-        public bool MatchesRule(int number)
-        {
-            return number % 3 == 0 && number % 5 == 0;
-        }
-
-        public string GetFizzBuzzString()
-        {
-            return "Fizzbuzz";
-        }
-    }
-
     public class FizzBuzz
     {
         private List<IRuleInterface> rules = new List<IRuleInterface>();
